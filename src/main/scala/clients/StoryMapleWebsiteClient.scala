f27aacd67fa51e0bdbd4ed027df872aeb2563538
Gonzalo Lepetic <lepeticg@gmail.com>
2021-07-14 13:02:11 -0300

    Added timeout for storymaple request

--- src/main/scala/clients/StoryMapleWebsiteClient.scala
+++ src/main/scala/clients/StoryMapleWebsiteClient.scala
@@ -3,17 +3,17 @@ package clients
 
 import config.HttpClientConfig
 
-import scalaj.http.Http
+import scalaj.http.{Http, HttpRequest}
 import spray.json._
 
 class StoryMapleWebsiteClient {
 
-  val host: String = HttpClientConfig.storymapleWebsiteHost
-  val rankings: String = HttpClientConfig.rankingsUrl
+  private val host: String = HttpClientConfig.storymapleWebsiteHost
+  private val rankings: String = HttpClientConfig.rankingsUrl
 
   def getRankings: Option[JsValue] = {
     try
-      Option(Http(host + rankings))
+      Option(this.buildRequest(this.rankings))
         .map(req => req.asString)
         .filter(res => res.is2xx)
         .map(res => res.body.parseJson)
@@ -22,8 +22,9 @@ class StoryMapleWebsiteClient {
         println(e.getMessage)
         None
     }
-
-
   }
+
+  private def buildRequest(url: String): HttpRequest = Http(host + url)
+    .timeout(HttpClientConfig.storyMapleTimeout, HttpClientConfig.storyMapleTimeout)
 
 }
--- src/main/scala/config/HttpClientConfig.scala
+++ src/main/scala/config/HttpClientConfig.scala
@@ -2,12 +2,14 @@ package org.maple
 package config
 
 import utils.OptionUtils._
-import BotEnvironment.getStringProperty
+
+import BotEnvironment.{getIntProperty, getStringProperty}
 
 object HttpClientConfig {
 
   val storymapleWebsiteHost: String = getStringProperty("storymaple.host").orThrow
   val rankingsUrl: String = getStringProperty("storymaple.rankings.search.url").orThrow
+  val storyMapleTimeout: Int = getIntProperty("storymaple.timeout").orThrow
 
   val worldTimeApiHost: String = getStringProperty("worldtime.api.host").orThrow
   val worldTimeApiTimezonesUrl: String = getStringProperty("worldtime.api.timezones.url").orThrow
