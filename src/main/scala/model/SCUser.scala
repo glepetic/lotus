f24dced13d248a7785ef4f0fafdb69812cb8660f
Gonzalo Lepetic <lepeticg@gmail.com>
2022-10-02 18:13:54 -0300

    Add logs

--- src/main/scala/model/SCUser.scala
+++ src/main/scala/model/SCUser.scala
@@ -15,8 +15,11 @@ case class SCUser(userId: String,
 
   def canDoLilynouch: Boolean = {
     val lastRolled = Option(lastRoll).map(_.atZone(ZoneId.systemDefault()))
+    println(s"Last Roll: $lastRolled")
     val now = Instant.now().atZone(ZoneId.systemDefault())
-    lastRolled.forall(lR => lR.getDayOfMonth < now.getDayOfMonth || ChronoUnit.DAYS.between(lR, now) > 1 && lR.isBefore(now))
+    println(s"Now: $now")
+    println(s"(${lastRolled.get.getDayOfMonth} < ${now.getDayOfMonth} || ${ChronoUnit.DAYS.between(lastRolled.get, now)}) && ${lastRolled.get.isBefore(now)}")
+    lastRolled.forall(lR => (lR.getDayOfMonth < now.getDayOfMonth || ChronoUnit.DAYS.between(lR, now) > 1) && lR.isBefore(now))
   }
 
   def scIncreaser: SCUser = SCUser(userId, serverId, lastRoll = Instant.now(), scCount + 1, donutCount, scrollCount, id)
