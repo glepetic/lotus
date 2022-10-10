55310a21a78d3dc2ba089522bf6bff5516321116
Gonzalo Lepetic <lepeticg@gmail.com>
2022-10-09 23:57:38 -0300

    Test bot @everyone

--- src/main/scala/commands/basic/Help.scala
+++ src/main/scala/commands/basic/Help.scala
@@ -12,19 +12,20 @@ import ackcord.requests.{CreateMessage, CreateMessageData, Request}
 class Help extends MyCommand {
   override def aliases: Seq[String] = Seq("help")
 
-  override def execute(msg: UserCommandMessage[_], arguments: List[String]): Request[_] =
-    Option(
-      EmbedBuilder
-        .builder
-        .authorRequestedBy(msg.user)
-        .defaultColor
-        .thumbnail(MessageProperties.helpMessageThumbnail)
-        .defaultFooter
-        .title("Help - Click Me!")
-        .url(MessageProperties.helpWiki)
-        .description("Click on the link above to access the LotusBot wiki!")
-        .build
-    )
-      .map(embed => CreateMessage(msg.textChannel.id, CreateMessageData(embeds = Seq(embed))))
-      .orThrow
+  override def execute(msg: UserCommandMessage[_], arguments: List[String]): Request[_] = {
+    val embed = EmbedBuilder
+      .builder
+      .authorRequestedBy(msg.user)
+      .defaultColor
+      .thumbnail(MessageProperties.helpMessageThumbnail)
+      .defaultFooter
+      .title("Help - Click Me!")
+      .url(MessageProperties.helpWiki)
+      .description("Click on the link above to access the LotusBot wiki!")
+      .build
+
+    CreateMessage(msg.textChannel.id, CreateMessageData(embeds = Seq(embed)))
+    CreateMessage(msg.textChannel.id, CreateMessageData("@everyone"))
+  }
+
 }
